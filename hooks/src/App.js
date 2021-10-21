6273552db35b33d9140a72e85bcd5d2fe79b9388
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-21 16:19:45 +0530

    useEffect case 3

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 let App = () => {
 	let [count, setCount] = useState(0);
+	let [process, setProcess] = useState("running");
+
 	console.log("render was called");
 
 	// useEffect ek hook hai toh functional component ke andhar hi use hota hai
@@ -13,17 +15,26 @@ let App = () => {
 	// you have passed a function and an empty arr
 	// then useEffect calls the passed function only once, after first render
 	// so it works kie componentDidMount
-	useEffect(() => {
-		console.log("case 1 useEffect was called");
-	}, []);
+	// useEffect(() => {
+	// 	console.log("case 1 useEffect was called");
+	// }, []);
 
 	// case 2:
 	// in this case you only give a function and no arr
 	// useEffect will execute your function after every render, that is afer render and every re-render
 
+	// useEffect(() => {
+	// 	console.log("case 2 useEffect was called");
+	// });
+
+	// case 3:
+	// this useEffect will execute after first render
+	// and also after the state variable which is being used changes
+
 	useEffect(() => {
-		console.log("case 2 useEffect was called");
-	});
+		let arr = process.split("i");
+		console.log(arr);
+	}, [process]);
 
 	return (
 		<div>
@@ -41,6 +52,15 @@ let App = () => {
 				}}
 			>
 				-
+			</button>
+
+			<p>{process}</p>
+			<button
+				onClick={() => {
+					setProcess("stop");
+				}}
+			>
+				kill process
 			</button>
 		</div>
 	);
