f3b2b85f1dc0cffb658c9ed767f5e8a7362c839e
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-21 12:22:24 +0530

    useEffect case 1

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let App = () => {
-	console.log("this will be called on every render");
-
-	// useState return array, us array ke andhar 2 cheeje hoti hai
-	// pehli us state ki value hoti hain
-	// dusri ek function hota => ye function se hum apne state ki value change kar sakte hain
-	
 	let [count, setCount] = useState(0);
+	console.log("render was called");
+
+	// useEffect ek hook hai toh functional component ke andhar hi use hota hai
+	// it takes 2 arguments => function , arr[optional]
+	// based on you have passed the arr or not
+	// we have 3 cases
+
+	// case:1
+	// you have passed a function and an empty arr
+	// then useEffect calls the passed function only once, after first render
+	// so it works kie componentDidMount
+	useEffect(() => {
+		console.log("use effect was called");
+	}, []);
+
 	return (
 		<div>
 			<button
