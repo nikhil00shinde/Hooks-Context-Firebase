95438be9fb0d750fe0abb44e573963ff04a57103
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-21 12:09:20 +0530

    useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 let App = () => {
 	console.log("this will be called on every render");
+
+	// useState return array, us array ke andhar 2 cheeje hoti hai
+	// pehli us state ki value hoti hain
+	// dusri ek function hota => ye function se hum apne state ki value change kar sakte hain
+	
 	let [count, setCount] = useState(0);
 	return (
 		<div>
