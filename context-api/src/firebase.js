b2305f84211e343d80e8864d235f7ca42ee1172d
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-22 18:16:16 +0530

    firebase basic setup

---
+++ context-api/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDqx6wUPZExcCdye3Rxr2ex0ou2YKBcIbU",
+	authDomain: "test-1-6751b.firebaseapp.com",
+	projectId: "test-1-6751b",
+	storageBucket: "test-1-6751b.appspot.com",
+	messagingSenderId: "210573345110",
+	appId: "1:210573345110:web:3617857b08db43fb3356f7",
+};
+
+firebase.initializeApp(firebaseConfig);
